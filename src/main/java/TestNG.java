a371f65daf51c7443a84400bf6b51f5ba9e4b8c4
eddgru6 <eddgru-6@student.ltu.se>
2022-05-02 15:36:08 +0200

    TestNG testing of calculator test

---
+++ src/main/java/TestNG.java
@@ -0,0 +1,17 @@
+public class TestNG {
+    public static int add(int i, int j){
+        return i + j;
+    }
+
+    public static int subtract(int i, int j){
+        return i - j;
+    }
+
+    public static int multiply(int i, int j){
+        return i * j;
+    }
+
+    public static int divide(int i, int j){
+        return i / j;
+    }
+}
