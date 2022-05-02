dfb3c570eeac2995e46376af5cc0fb8210f49941
eddgru6 <eddgru-6@student.ltu.se>
2022-05-02 16:22:33 +0200

    Finishing touches

--- src/test/java/TestNGTest.java
+++ src/test/java/TestNGTest.java
@@ -2,23 +2,21 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TestNGTest {
-    @Test()
-    public void Add() {
-        Assert.assertEquals(5, TestNG.add(2,3));
-    }
+    @Test(testName = "Add")
+    public void testAdd() { Assert.assertEquals(5, TestNG.add(2,3));}
 
-    @Test()
-    public void Subtract() {
+    @Test(testName = "Subtract")
+    public void testSubtract() {
         Assert.assertEquals(2, TestNG.subtract(3,1));
     }
 
-    @Test()
-    public void Multiply() {
+    @Test(testName = "Multiply")
+    public void testMultiply() {
         Assert.assertEquals(10, TestNG.multiply(5,2));
     }
 
-    @Test()
-    public void Divide() {
+    @Test(testName = "Divide")
+    public void testDivide() {
         Assert.assertEquals(10, TestNG.divide(100,10));
     }
 }
